0679e5edf8556bb7e846543377b0e1d1a49e4a9d
Nicolas Turner <nicolasrturner@gmail.com>
2018-10-13 22:03:08 -0700

    Deployed to Heroku App is live and added linter

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyparser = require('body-parser');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
-  
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +16,7 @@ app.post('/api/notifications/send', (req, res) => {
       user: process.env.EMAIL,
       pass: process.env.PASS,
     },
-  })
+  });
 
   const mailOptions = {
     from: process.env.EMAIL,
@@ -25,25 +24,27 @@ app.post('/api/notifications/send', (req, res) => {
     subject: 'TEST TEST ALERT ALERT',
     text: 'THIS IS A TEST OF THE LOCAL ALERT NETWORK!',
     html: '<b>Hello World</b>'
-  }
+  };
   transporter.sendMail(mailOptions, (err, res) => {
     if (err) {
       console.log('Failed to send email', err);
     } else {
       console.log('Email succesfully sent!', res);
     }
-  })
-})
-
+  });
+});
 
 app.get('/api/notifications', (req, res) => {
   console.log('USer is real!', process.env.EMAIL);
   res.json({
     confirmation: 'success',
     message: 'it worked!',
-    user: process.env.EMAIL,
   });
-})
+});
+
+app.get('/', (req, res) => {
+  res.send('Alert Tracker is Live! Keeping you informed about every alert...');
+});
 
 app.listen(PORT, () => {
   console.log(`Hercules is here for you on port ${PORT}`);
