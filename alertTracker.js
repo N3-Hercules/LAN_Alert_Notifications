29325d17b7579f307af30c26c17ab0cc1c935ff0
Nicolas Turner <nicolasrturner@gmail.com>
2018-10-15 21:42:13 -0700

    Refactored alertTracker to check if friends are impacted by alert

--- alertTracker.js
+++ alertTracker.js
@@ -1,32 +1,48 @@
-// Helpers
-const extractUserInfo = (obj) => {
-  let key = Object.keys(obj);
-  let name = obj[key].name;
-  let email = obj[key].email;
-  let picture = obj[key].picture;
-  return [name, email, picture];
-}
-
-const location = (obj) => {
-  let key = Object.keys(obj);
-  let latitude = obj[key].homeLat;
-  let longitude = obj[key].homeLong;
-  return [latitude, longitude];
-} 
-
-const alertLocations = (obj) => {
-  let key = Object.keys(obj);
-  let lat = obj[key].latitude;
-  let long = obj[key].longitude;
-  return [lat, long];
-}
-
-const alertDetails = (obj) => {
-  let key = Object.keys(obj);
-  let category = obj[key].category;
-  let photo = obj[key].url;
-  return [category, photo];
-}
+let sample = [ [ { id: 1,
+  name: 'Nicolas Turner',
+  email: 'nicolasrturner@gmail.com',
+  provider: 'google',
+  provider_id: 2147483647,
+  picture: 'https://lh6.googleusercontent.com/-l_N2cyMTHkE/AAAAAAAAAAI/AAAAAAAAAAs/gKIXkyIgaAs/s96-c/photo.jpg',
+  token: 0,
+  homeLat: '47.72155850000000000000',
+  homeLong: '-122.19219729999999000000',
+  createdAt: '2018-10-13T19:27:56.000Z',
+  updatedAt: '2018-10-14T02:02:01.000Z' } ],
+[ { id: 871,
+  latitude: '47.72154930000000000000',
+  longitude: '-122.19222980000000000000',
+  notes: '',
+  category: 'tsunami',
+  url: null,
+  photoTag: '',
+  createdAt: '2018-10-16T02:46:07.000Z',
+  updatedAt: '2018-10-16T02:46:07.000Z',
+  EventId: null,
+  UserId: null } ],
+[ { id: 11,
+  name: 'Nathan Vang',
+  email: 'vang.nathany@gmail.com',
+  provider: 'google',
+  provider_id: 2147483647,
+  picture: 'https://lh3.googleusercontent.com/-8xSRfi__hF0/AAAAAAAAAAI/AAAAAAAAAAA/AAN31DVAndCid6xmJab1eFvt0zRcJIjZIg/s96-c/photo.jpg',
+  token: 0,
+  homeLat: '37.72235900000000000000',
+  homeLong: '-122.15841540000000000000',
+  createdAt: '2018-10-13T19:29:12.000Z',
+  updatedAt: '2018-10-13T19:30:36.000Z' },
+{ id: 41,
+  name: 'nathan ong',
+  email: 'nathan.d.ong@gmail.com',
+  provider: 'google',
+  provider_id: 2147483647,
+  picture: 'https://lh5.googleusercontent.com/-YsUz_PcvkmY/AAAAAAAAAAI/AAAAAAAAAB4/DMaf7nsMRm4/s96-c/photo.jpg',
+  token: 0,
+  homeLat: '37.72235900000000000000',
+  homeLong: '-122.15841540000000000000',
+  createdAt: '2018-10-13T22:41:15.000Z',
+  updatedAt: '2018-10-15T15:19:05.000Z' } ] ]
+// New data flow schema = user, alert, friends[?]
 
 const distance = (lat1, lon1, lat2, lon2) => {
   const radlat1 = Math.PI * lat1 / 180;
@@ -46,20 +62,75 @@ const distance = (lat1, lon1, lat2, lon2) => {
 
 // Is event near user
 const dataParser = (object) => {
-  let keys = Object.keys(object);
-  const userData = object[keys[0]];
-  const friendData = object[keys[1]];
-  const alertData = object[keys[2]];
-   
-  const user = extractUserInfo(userData);
-  const userLoc =  location(userData);
-  const alertLoc = alertLocations(alertData);
-  const alert = alertDetails(alertData);
+  const user = {
+    name: '',
+    email: '',
+    homeLat: '',
+    homeLong: '',
+    impacted: false,
+  };
+  // Extract main user data
+  JSON.stringify(object[0], (key, value) => {
+    if (key === 'name') return user.name = value;
+    if (key === 'email') return user.email = value;
+    if (key === 'homeLat') return user.homeLat = value;
+    if (key === 'homeLong') return user.homeLong = value;
+    return value;
+  });
 
-  if (distance(alertLoc[0], alertLoc[1], userLoc[0], userLoc[1]) < 10) {
-    return user.concat(alert);
+  const friends = {
+    users: [],
+    emails: [],
+    homeLat: [],
+    homeLong: [],
+    impacted: false,
+  };
+
+  const data = {
+    category: [],
+    latitude: [],
+    longitude: [],
+  };
+
+  JSON.stringify(object, (key, value) => {
+    if (key === 'name') return friends.users.push(value);
+    if (key === 'email') return friends.emails.push(value);
+    if (key === 'homeLat') return friends.homeLat.push(value);
+    if (key === 'homeLong') return friends.homeLong.push(value);
+    if (key === 'category') return data.category.push(value);
+    if (key === 'latitude') return data.latitude.push(value);
+    if (key === 'longitude') return data.longitude.push(value);
+    return value;
+  });
+
+  friends.users.shift();
+  friends.emails.shift();
+  friends.homeLat.shift();
+  friends.homeLong.shift();
+
+  if (distance(data.latitude, data.longitude, user.homeLat, user.homeLong) < 10) {
+    console.log('User is impacted');
+    user.impacted = true;
+    console.log(user);
+  }
+
+  if (friends.length) {
+    for (let i = 0; i < data.homeLat.length; ++i) {
+      if (distance(data.latitude, data.longitude, data.homeLat, data.homeLong) < 10) {
+        friends.impacted = true;
+      }
+    }
+  }
+
+  if (user.impacted) {
+    return { user, data };
+  }
+  if (friends.impacted) {
+    return { user, data, friends };
   }
   return null;
 };
+
+console.log(dataParser(sample));
 
 exports.dataParser = dataParser;
